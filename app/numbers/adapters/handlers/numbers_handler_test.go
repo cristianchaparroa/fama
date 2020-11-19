16414b424b14108d9c8dbaa521da6e31f3733612
cristianchaparroa <cristianchaparroa@gmail.com>
2020-11-18 23:17:46 -0500

    testing: add unit testing for not supported lang requested

--- app/numbers/adapters/handlers/numbers_handler_test.go
+++ app/numbers/adapters/handlers/numbers_handler_test.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
+	"fama/core/handlers"
 	"fama/numbers/ports/mocks"
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -85,4 +87,32 @@ func (s *numbersHandlerSuite) TestNumbersHandler_ToWords_OK_With_Lang() {
 	s.Equal(StatusOK, response.Status)
 	s.Equal(expectedWords, response.Words)
 	s.Equal(expectedLang, response.Lang)
+}
+
+func (s *numbersHandlerSuite) TestNumbersHandler_ToWords_WithoutSupportedLang() {
+	recorder := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(recorder)
+	url := fmt.Sprintf("/numbers/%s/words?lang=de", "12")
+	c.Request, _ = http.NewRequest(http.MethodGet, url, nil)
+	c.Params = []gin.Param{
+		{
+			Key:   "number",
+			Value: "12",
+		},
+	}
+	expectedWords := ""
+	expectedError := "lang not supported"
+	s.manager.Mock.On("ToWords", mock.Anything, mock.Anything).
+		Return(expectedWords, errors.New(expectedError)).
+		Once()
+
+	s.handler.ToWords(c)
+	s.Equal(http.StatusUnprocessableEntity, recorder.Code)
+
+	var errResponse handlers.Error
+	err := json.NewDecoder(recorder.Body).Decode(&errResponse)
+	s.Nil(err)
+
+	s.Equal(http.StatusUnprocessableEntity, errResponse.Status)
+	s.Equal(expectedError, errResponse.Title)
 }
